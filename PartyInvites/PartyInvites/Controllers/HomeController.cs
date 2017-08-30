391c90ca712efa4aa73bbe6cfdd7b8db3ab7cb0c
Koen Kamman <kammankoen@gmail.com>
2017-08-30 23:18:02 +0200

    Listing 2-13

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
+
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
         }
+
+        [HttpGet]
         public ViewResult RsvpForm()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: store repsonse from guest
             return View();
         }
     }
