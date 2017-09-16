33b7cd665a6c25aa0642ed3c537555587aea3dbd
Koen Kamman <kammankoen@gmail.com>
2017-09-16 21:33:36 +0200

    Counter Widget

---
+++ PartyInvites/PartyInvites/Components/CounterViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Abstract;
+using PartyInvites.Models.ViewModels;
+
+namespace PartyInvites.Components
+{
+    public class CounterViewComponent : ViewComponent
+    {
+	    private readonly IRepository _repository;
+
+	    public CounterViewComponent(IRepository repository)
+	    {
+		    _repository = repository;
+	    }
+
+	    public IViewComponentResult Invoke()
+	    {
+		    var responses = _repository.GetAllResponses().ToList();
+		    return View(new CounterViewModel()
+		    {
+			    ResponseAmount = responses.Count(),
+				AttendingAmount = responses.Count(r => r.WillAttend == true)
+		    });
+	    }
+    }
+}
